2609b9c4c29632545c988d98b38ebd7f90e7e7d2
PabloRR100 <pabloruizruiz10@gmail.com>
2019-01-29 10:11:50 +0100

    sorting with buble sort

--- Interview_Questions/1_Arrays/03_new_year_caos.py
+++ Interview_Questions/1_Arrays/03_new_year_caos.py
@@ -9,5 +9,37 @@ Created on Sun Jan 27 17:43:46 2019
 """
 
 input = list(map(int, '2 1 5 3 4'.split()))
-input = list(map(int, '2 5 1 3 4'.split()))
+#input = list(map(int, '2 5 1 3 4'.split()))
+
+def NewYearChaos(queue):
+    lastIndex = len(queue) - 1
+    swaps = 0
+    comps = 0
+    swaped = False
+    
+    def swap(x,y):
+        return y,x
+    
+    # check if the queue is too chaotic
+    # a number can't be more than two positions away from its index
+    for i, v in enumerate(queue):
+        if (v - 1) - i > 2:
+            return "Too chaotic"
+        
+    # bubble sorting to find the answer
+    for i in range(0, lastIndex):
+        for j in range(0, lastIndex):
+            comps += 1
+            if queue[j] > queue[j+1]:
+                queue[j], queue[j+1] = swap(queue[j], queue[j+1])
+                swaps += 1
+                swaped = True
+        
+        # leave sorting if the rest is already sort! (Timeout errors)
+        if swaped: swaped = False
+        else: break
+    
+    return swaps
+
+NewYearChaos(input)
 
