f6ac32eebabcaa8a7f08f706b1f451c21300b940
PabloRR100 <pabloruizruiz10@gmail.com>
2019-02-20 17:55:18 +0100

    suffering with non-recursive bds with stacks

--- Cracking the Coding Interview/04_Trees_and_Graphs/01_route_between_nodes.py
+++ Cracking the Coding Interview/04_Trees_and_Graphs/01_route_between_nodes.py
@@ -30,38 +30,64 @@ from trees_and_graphs import Node, Graph, Graph_AdjList
 ### Graph as a collection of nodes
 # --------------------------------
 
-vertices = [0,1,2,3,4,5]
-connections = [(0, 1),(0,2),(1,3),(1,4),(4,2),(2,5)]
+example = 'long'
 
-nodes = [Node(n) for n in vertices]
+if example == 'short':
+    vertices = [0,1,2,3,4,5]
+    connections = [(0, 1),(0,2),(1,3),(1,4),(4,2),(2,5)]
 
-graph = Graph(len(vertices))
-for node in nodes:
-    graph.addNode(node)
+if example == 'long':
+    vertices = [0,1,2,3,4,5,6,7,8,9,10,11]
+    connections = [(0, 1),(1,2),(1,3),(2,4),(2,5),(3,7),(5,6),(7,6),(8,1),(8,3),(8,9),(9,10),(9,11)]
+
+
+def create_graph(vertices, connections):
     
-for path in connections:
-    graph.addEdge(graph.nodes[path[0]], graph.nodes[path[1]])
-   
-graph_ch = [graph]    
-    
+    nodes = [Node(n) for n in vertices]
+    graph = Graph(len(vertices))
+    for node in nodes:
+        graph.addNode(node)
+        
+    for path in connections:
+        graph.addEdge(graph.nodes[path[0]], graph.nodes[path[1]])
+    return graph
 
+    
 ## Depth First Search for a collection of nodes
 
-def DFS(root:Node, destiny:Node):
+def DFS(root:Node, goal:Node):
     
-    # print('Checking {} -> {}'.format(root.name, destiny.name))
-    if root == destiny: print('Path found'); return
+    print('Checking {} -> {}'.format(root.name, goal.name))
+    if root == goal: print('Path found!')
     
     root.visited = True
     for node in root.adjacents:
         if not node.visited:
             node.visited = True
-            DFS(node, destiny)
+            DFS(node, goal)
+    
+
+if example == 'short':
+    graph = create_graph(vertices, connections)
+    DFS(graph.nodes[1], graph.nodes[5]) # B -> S: True   for short example
+    
+    graph = create_graph(vertices, connections)
+    DFS(graph.nodes[4], graph.nodes[5]) # E -> S: True
+    
+    graph = create_graph(vertices, connections)
+    DFS(graph.nodes[3], graph.nodes[5]) # D -> S: False
 
 
-#DFS(nodes[1], nodes[5]) # B -> S: True
-#DFS(nodes[4], nodes[5]) # E -> S: True
-#DFS(nodes[3], nodes[5]) # D -> S: False
+if example == 'long':
+    graph = create_graph(vertices, connections)
+    DFS(graph.nodes[1], graph.nodes[6]) # 1 -> 6: True
+    
+    graph = create_graph(vertices, connections)
+    DFS(graph.nodes[9], graph.nodes[7]) # 9 -> 7: False
+    
+    graph = create_graph(vertices, connections)
+    DFS(graph.nodes[8], graph.nodes[5]) # 8 -> 5: True
+
 
 
 ## Breadth First Search for a collection of nodes
@@ -77,19 +103,21 @@ def BFS(root:Node, destiny:Node):
 
 #vertices = ['A','B','C','D','E','S']
 #connections = [('A', 'B'),('A','C'),('B','D'),('B','E'),('E','C'),('C','S')]
-vertices = [0,1,2,3,4,5]
-connections = [(0, 1),(0,2),(1,3),(1,4),(4,2),(2,5)]
 
-graph_adjL = Graph_AdjList(len(vertices))
-for path in connections:
-    graph_adjL.addEdge(path[0],path[1])
+
+def create_graph_AdjList(vertices, connections):
+    graph_adjL = Graph_AdjList(len(vertices))
+    for path in connections:
+        graph_adjL.addEdge(path[0],path[1])
+    return graph_adjL
     
+graph_adjL = create_graph_AdjList(vertices, connections)
 graph_adjL_ch = graph_adjL.nodes
 
 
 ## Depth First Search for a default dict
 
-def DFS_(graph:Graph, root:Node, destiny:Node):    
+def DFS_(graph:Graph, root:Node, goal:Node):    
     '''
     path: Stack which keeps track of the current path during search
     visited: Boolean Array which keeps track if the node[index] has been visited
@@ -97,34 +125,53 @@ def DFS_(graph:Graph, root:Node, destiny:Node):
     
     visited = [False] * graph.N    
 
-    path = [root]
+    path = []
+    stack = [root]
     visited[root] = True 
-    
-    for n in graph.nodes[node]:
         
-        while path:
+    while stack:
         
-            node = path.pop()
-            to_explore = graph.nodes[node] 
+        node = stack.pop()
+        if node in path:
+            continue
+        
+            path.append(node)
+            for n in graph.nodes[node]:
+                
+                stack.append(n)
             
-            if destiny == n: 
-                return True
-            
-            if not visited[n]:
-                visited[n] = True
-                path.append(n)
                 
     return False
-                
-            
-        
-        
-DFS_(graph_adjL, 1, 5) # B -> S: False
-
+                    
+DFS_(graph_adjL, 1, 6) # 1 -> 6: True
+DFS_(graph_adjL, 9, 7) # 9 -> 7: False
+DFS_(graph_adjL, 8, 5) # 8 -> 5: True
 
 
 
 ## Breadth First Search for a default dict
 
-def BFS_(graph):
-    pass
+def BFS_(graph:Graph, root:Node, goal:Node):
+    
+    visited = [False] * graph.N    
+
+    stack = [root]
+    visited[root] = True 
+        
+    while stack:
+        
+        node = stack.pop()
+        if node == goal: print('Path found!'); return True
+        for n in graph.nodes[node]:
+            
+            print('Moving {} -> {}'.format(node, n))            
+            if visited[n] == False:
+                visited[n] = True
+                stack.append(n)
+                
+    return False
+
+
+BFS_(graph_adjL, 1, 6) # 1 -> 6: True
+BFS_(graph_adjL, 9, 7) # 9 -> 7: False
+BFS_(graph_adjL, 8, 5) # 8 -> 5: True
