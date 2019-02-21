4a6cf2ec128c1107cde3195df8c240239bde097b
PabloRR100 <pabloruizruiz10@gmail.com>
2019-02-21 18:10:44 +0100

    found wrong implementation on printing a binary tree by level

--- Cracking the Coding Interview/04_Trees_and_Graphs/print_binary_tree_by_level.py
+++ Cracking the Coding Interview/04_Trees_and_Graphs/print_binary_tree_by_level.py
@@ -27,18 +27,19 @@ binary_tree = assign_values(array)
 # Find solution
 def print_by_level(root, N):
     
+    counter = 0
     queue = [root]
     while queue:
         
-        print('Queue: ', queue)
         current = queue.pop()
-        print('Queue {} -> Current {}'.format([x.value for x in queue], current.value))
+        print('[{}]: {}'.format(counter, current.value))
         
         if current.left:
             queue.insert(0,current.left)
         if current.right:
             queue.insert(0,current.right)
             
+        counter += 1
 
 print_by_level(binary_tree, len(array))
         
