ef1b3dd2ccc261d714fa34a613f665cfc216a44c
PabloRR100 <pabloruizruiz10@gmail.com>
2019-02-21 08:56:28 +0100

    iterative implementations of dfs and bfs ready -> left nodes collections for bfs

--- Cracking the Coding Interview/04_Trees_and_Graphs/01_route_between_nodes.py
+++ Cracking the Coding Interview/04_Trees_and_Graphs/01_route_between_nodes.py
@@ -14,6 +14,7 @@ There is two possibilities:
             * allows to use the name of the node as the key and the list of all of the adjacent as teh values
             * feels pythonic
             * we can't check a whole level at the same time since one key holds the list of all adjacents
+            * graph don't get modify when searching and don't need to be re-initialize for another search
 
 Equivalence Table between index and node names:
     - 0: A
@@ -24,17 +25,26 @@ Equivalence Table between index and node names:
     - 5: S
 """
 
+'''
+DFS can be implemented on its own in 2 ways:
+    
+    [1]: Recursively
+    [2]: Iteratively using a stack
+
+BFS is implemented using a queue
+'''
+
 from trees_and_graphs import Node, Graph, Graph_AdjList
 
 
 ### Graph as a collection of nodes
 # --------------------------------
 
-example = 'short'
+example = 'long'
 
 if example == 'short':
-    vertices = [0,1,2,3,4,5]
-    connections = [(0, 1),(0,2),(1,3),(1,4),(4,2),(2,5)]
+    vertices = [0,1,2,3,4,5,6]
+    connections = [(0, 1),(0,2),(1,3),(1,4),(2,5),(3,6),(4,2)]
 
 if example == 'long':
     vertices = [0,1,2,3,4,5,6,7,8,9,10,11]
@@ -57,12 +67,12 @@ def create_graph(vertices, connections):
 
 def DFS(root:Node, goal:Node):
     
-    print('Checking {} -> {}'.format(root.name, goal.name))
-    if root == goal: print('Path found!')
+    if root == goal: print('Path found!'); return
     
     root.visited = True
     for node in root.adjacents:
         if not node.visited:
+            print('{} -> {}'.format(root.name, node.name)) # Print underlaying path
             node.visited = True
             DFS(node, goal)
     
@@ -123,6 +133,7 @@ def DFS_(graph:Graph, root:Node, goal:Node):
     visited: Boolean Array which keeps track if the node[index] has been visited
     '''
     
+    print('\n\nDepth First Search\n')
     visited = [False] * graph.N    
 
     path = []
@@ -131,21 +142,28 @@ def DFS_(graph:Graph, root:Node, goal:Node):
         
     while stack:
         
-        node = stack.pop()
-        if node in path:
-            continue
+        current = stack.pop()
+        path.append(current)
+        print(current)
+        if current == goal: print('Path found!'); return True
         
-            path.append(node)
-            for n in graph.nodes[node]:
-                
-                stack.append(n)
-            
-                
+        for neighb in graph.nodes[current]:
+            if not visited[neighb]:
+                stack.append(neighb)
+                visited[neighb] = True
+        
+    print('Path not found!')
     return False
-                    
-DFS_(graph_adjL, 1, 6) # 1 -> 6: True
-DFS_(graph_adjL, 9, 7) # 9 -> 7: False
-DFS_(graph_adjL, 8, 5) # 8 -> 5: True
+            
+if example == 'short':
+    DFS_(graph_adjL, 0, 3) # 0 -> 3: True  --> Perfect example of finding longest path
+    DFS_(graph_adjL, 4, 5) # 4 -> 5: True
+    DFS_(graph_adjL, 3, 5) # 3 -> 5: False
+        
+if example == 'long':
+    DFS_(graph_adjL, 1, 6) # 1 -> 6: True
+    DFS_(graph_adjL, 9, 7) # 9 -> 7: False
+    DFS_(graph_adjL, 8, 5) # 8 -> 5: True  --> Another great example of searching too much
 
 
 
@@ -153,35 +171,37 @@ DFS_(graph_adjL, 8, 5) # 8 -> 5: True
 
 def BFS_(graph:Graph, root:Node, goal:Node):
     
+    print('\n\nBreadth First Search\n')
     visited = [False] * graph.N    
 
-    stack = [root]
+    queue = [root]
     visited[root] = True 
-    path_explored = [root]
         
-    while stack:
+    while queue:
         
-        print('Stack', stack)
-        node = stack.pop()
-        
-        print('Current', node)
-        
-        if node == goal: 
-            print('Path found!'); print('Path explored: ', path_explored); return True
-        for n in graph.nodes[node]:
+        print('Queue', queue)
+        current = queue.pop()
+        print('Current', current)
+                
+        if current == goal: 
+            print('Path found!'); return True
+            
+        for n in graph.nodes[current]:
             
             if visited[n] == False:
-                visited[n] = True
-                stack.append(n)
-                path_explored.append(n)
                 
-    print('Path explored: ', path_explored)
+                queue.insert(0,n)
+                visited[n] = True
+                
+    print('Path not found!')
     return False
 
 if example == 'short':
-    BFS_(graph_adjL, 0, 3) # 1 -> 6: True
-
-    
-    
-BFS_(graph_adjL, 9, 7) # 9 -> 7: False
-BFS_(graph_adjL, 8, 5) # 8 -> 5: True
+    BFS_(graph_adjL, 0, 3) # 0 -> 3: True  --> Perfect example of finding longest path
+    BFS_(graph_adjL, 4, 5) # 4 -> 5: True
+    BFS_(graph_adjL, 3, 5) # 3 -> 5: False
+        
+if example == 'long':
+    BFS_(graph_adjL, 1, 6) # 1 -> 6: True
+    BFS_(graph_adjL, 9, 7) # 9 -> 7: False
+    BFS_(graph_adjL, 8, 5) # 8 -> 5: True  --> Another great example of searching too much
