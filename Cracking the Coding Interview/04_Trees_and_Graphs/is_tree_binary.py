bace0a0734b5636c0eaffe1552b42e87453894f5
PabloRR100 <pabloruizruiz10@gmail.com>
2019-02-23 10:32:01 +0100

    tree binary search

--- Cracking the Coding Interview/04_Trees_and_Graphs/is_tree_binary.py
+++ Cracking the Coding Interview/04_Trees_and_Graphs/is_tree_binary.py
@@ -10,45 +10,29 @@ left and < than any node on the right
 from trees_and_graphs import TreeNode as Node
 
 
-# Example of balanced tree
+# Example of binary search tree     ## Uncommente these for not binary search tree
 root = Node(8)
 root.left = Node(4)
 root.left.left = Node(3)
+#root.left.left.right = Node(1)      ## Remove this
 root.left.right = Node(5)
 root.right = Node(13)
 root.right.left = Node(11)
 root.right.left.right = Node(12)
 root.right.right = Node(15)
-root.right.right.right = Node(14)
-
-# Example of unbalanced tree
+#root.right.right.right = Node(14)   ## Remove thiss
 
 
-def eval_Node(node):
+def is_Balanced(node, min_, max_):
     
-    global overal
-    children = list()
-    left, right = node.left, node.right
-    if left: children.append(left)
-    if right: children.append(right)
-    children_ = [n.value for n in children]
+    if node == None: return True
+    if (node.value < min_ or node.value > max_): 
+        return False
     
-    if (left and right):
-        if not ((node.left.value <= node.value) and (node.right.value > node.value)):
-            return False
-        
-    elif left and not right:
-        if not (node.left.value <= node.value):
-            return False
-        
-    elif right and not left:
-        if not (node.right.value > node.value):
-            return False
-    
-    for child in children:
-        eval_Node(child)
-          
-    return True
+    return is_Balanced(node.left, min_, node.value) and is_Balanced(node.right, node.value, max_)
 
-eval_Node(root)
+from math import inf
+is_Balanced(root, -inf, inf)
+
+
 
