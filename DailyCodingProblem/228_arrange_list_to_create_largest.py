e87c59bcdecd431c29b9f80a893763bbf291f1c0
PabloRR100 <pabloruizruiz10@gmail.com>
2020-01-17 20:39:56 +0100

    arranged list to form largest concatenated int

--- DailyCodingProblem/228_arrange_list_to_create_largest.py
+++ DailyCodingProblem/228_arrange_list_to_create_largest.py
@@ -9,6 +9,19 @@ largest possible integer.
 For example, given [10, 7, 76, 415], you should return 77641510.
 '''
 
-def arrange_to_largest(array):
-    return num
+def arrange_to_largest(nums):
 
+    nums = list(map(str, nums))
+    max_len = len(max(nums, key=len))
+    norm = [n*(max_len//len(n)) + n[:(max_len%len(n))] for n in nums]
+    # norm = list()
+    # for n in nums:
+    #     t,r = divmod(max_len, len(n))
+    #     norm.append(n*t + n[:r])
+    both = [(r,n) for r,n in zip(nums,norm)]
+    results = reversed(sorted(both, key=lambda s: s[1]))
+    return int(''.join(r[0] for r in results))
+
+
+nums = [10, 7, 76, 415]
+arrange_to_largest(nums)
