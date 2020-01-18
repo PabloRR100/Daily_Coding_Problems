93db14180732842413c33e8698a9cf30a9b2c13c
PabloRR100 <pabloruizruiz10@gmail.com>
2020-01-18 10:47:14 +0100

    first part of array manipulation

---
+++ Youtube/Irfan_Baqui/array_manipulation.py
@@ -0,0 +1,33 @@
+
+
+''' Move all zeros of an array to the back '''
+
+def not_inplace_manipulation(arr):
+    zeros = 0
+    new_arr = list()
+    for n in arr:
+        if n == 0:
+            zeros += 1
+        else:
+            new_arr.append(n)
+    return new_arr + [0] * zeros
+
+
+def inplace_manipulation(arr):
+
+    def swap(arr,i,j):
+        arr[i], arr[j] = arr[j], arr[i]
+        return arr
+
+    for i,n1 in enumerate(arr):
+        if n1 == 0:
+            for j,n2 in enumerate(arr[i+1:]):
+                if n2 != 0:
+                    break
+            arr = swap(arr,i,i+j)
+    
+    return arr
+
+arr = [1,2,0,'a',0,6]
+not_inplace_manipulation(arr)
+inplace_manipulation(arr)
