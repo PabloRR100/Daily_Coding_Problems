23c29fcd74ad538ec8409f9d1df4226dcab9a608
PabloRR100 <pabloruizruiz10@gmail.com>
2020-01-19 17:14:15 +0100

    rec

--- Youtube/CS_Dojo/ways_of_decode_message.py
+++ Youtube/CS_Dojo/ways_of_decode_message.py
@@ -4,12 +4,28 @@ We have a mapping letter -> number.
 For a message as a big number, how many ways to decode it?
 '''
 
-from string import ascii_letters
+from string import ascii_lowercase
 from collections import defaultdict
 
 mapping = defaultdict()
-for n,l in enumerate(ascii_letters):
-    mapping[l] = str(n)
+for n,l in enumerate(ascii_lowercase):
+    mapping[l] = str(n+1)
 mapping = dict(mapping)
 
-input = '123'
+input = '123' 
+
+def n_ways(msg:str):
+
+    if len(msg) == 0:
+        return 0
+
+    if len(msg) == 1:
+        return 1
+
+    if msg[:2] > '26':
+        return n_ways(msg[1:])
+    
+    else:
+        return n_ways(msg[1:]) + n_ways(msg[2:])
+
+n_ways(input)
